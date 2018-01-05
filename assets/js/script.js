84563487a55cd11c5742c2829b32a6a0872f3ad2
malcodeman <malcodeman@users.noreply.github.com>
2018-01-05 19:39:35 +0100

    More info

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,15 @@ function getCurrentWeather() {
     weather.getCurrentWeather("london").then(response => {
             console.log(response);
             let temp = response.main.temp;
-            document.getElementById("temperature").textContent = unitConverter.kelvinToCelsius(temp)
+            let humidity = response.main.humidity;
+            let description = response.weather[0].description;            
+            document.getElementById("temperature").textContent = unitConverter.kelvinToCelsius(temp);
+            document.getElementById("humidity").textContent = humidity;
+            document.getElementById("description").textContent = description;
         })
         .catch(error => {
             console.log(error.message);
         })
 }
 
-getCurrentWeather();
+getCurrentWeather(); 
