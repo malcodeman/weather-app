5f71be51e1fc1fe2bcc2b52c524b2a5e2958d1a2
malcodeman <malcodeman@users.noreply.github.com>
2018-01-07 21:58:04 +0100

    Simplified main, get weather data by location

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,28 @@ const windowCommands = require("./assets/js/windowCommands");
 const geolocation = require("./assets/js/geolocation");
 const timeanddate = require("./assets/js/timeAnDdate");
 
-function getCurrentWeather() {
-    weather.byCityName("london").then(response => {
-            console.log(response);
-            let temp = response.main.temp;
-            let description = response.weather[0].description;
-            let city = response.name;
-            let country = response.sys.country;
-            document.getElementById("image_left").src = unsplash.randomFromSearchTerm(response.weather[0].main);
-            document.getElementById("image_right").src = unsplash.randomFromSearchTerm(city);
-            document.getElementById("temperature").textContent = unitConverter.kelvinToCelsius(temp);
-            document.getElementById("description").textContent = description;
-            document.getElementById("location").textContent = city + ", " + country;
-        })
-        .catch(error => {
-            console.log(error.message);
-        })
-}
-
-function getCurrentPosition() {
+function main() {
+    document.getElementById("close").addEventListener("click", () => {
+        windowCommands.close();
+    })
     geolocation.currentPosition().then(response => {
-            console.log("latitude " + response.coords.latitude);
-            console.log("longitude " + response.coords.longitude);
+            let lat = response.coords.latitude;
+            let lon = response.coords.longitude;
+            weather.byGeographicCoordinates(lat, lon)
+                .then(response => {
+                    let temp = response.main.temp;
+                    let description = response.weather[0].description;
+                    let city = response.name;
+                    let country = response.sys.country;
+                    document.getElementById("description").textContent = description;
+                    document.getElementById("location").textContent = city + ", " + country;
+                    document.getElementById("temperature").textContent = Math.round(unitConverter.kelvinToCelsius(temp) * 10) / 10 + "°";
+                    document.getElementById("image_left").src = unsplash.randomFromSearchTerm(response.weather[0].main);
+                    document.getElementById("image_right").src = unsplash.randomFromSearchTerm(city + ", city");
+                })
+                .catch(error => {
+                    console.log(error.message);
+                });
         })
         .catch(error => {
             console.log(error.message);
@@ -37,14 +37,5 @@ function renderDate() {
     document.getElementById("date").textContent = timeanddate.dayOfTheMonth() + " " + timeanddate.monthAbbreviaton();
 }
 
-getCurrentWeather();
 renderDate();
-getCurrentPosition();
-
-function changeBackgroundImage(url) {
-    document.getElementById("page").style.backgroundImage = "url(" + url + ")";
-}
-
-document.getElementById("close").addEventListener("click", () => {
-    windowCommands.close();
-})
+main();
